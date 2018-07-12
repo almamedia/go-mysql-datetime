06abe859ed3eeafe64d337ad2d55e5af003005c2
Ari Makela <ari.makela@iki.fi>
2018-07-12 15:03:01 +0300

    Added function hello.HelloString()

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,13 @@
 package hello
 
+import "fmt"
+
 // Hello returns string "hello, world"
 func Hello() string {
 	return "hello, world"
+}
+
+// HelloString says hello to the parameter
+func HelloString(hello string) string {
+	return fmt.Sprintf("hello, %s")
 }
